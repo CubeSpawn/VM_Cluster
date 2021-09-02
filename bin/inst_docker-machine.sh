1c7edccf01d6e9ed1543a2f4e170c421b2d1d86c
CubeSpawn <cubespawn@gmail.com>
2021-09-02 03:07:02 -0500

    first edits

---
+++ bin/inst_docker-machine.sh
@@ -0,0 +1,5 @@
+#From https://github.com/docker/machine/releases/
+
+curl -L https://github.com/docker/machine/releases/download/v0.16.2/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine &&
+    chmod +x /tmp/docker-machine &&
+    sudo cp /tmp/docker-machine /usr/local/bin/docker-machine
