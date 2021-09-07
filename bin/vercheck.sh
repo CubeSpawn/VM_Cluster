ff84048a78183900ced2a16d9fa93db9ffd1c825
CubeSpawn <cubespawn@gmail.com>
2021-09-07 18:37:33 -0500

    tweaks

--- bin/vercheck.sh
+++ bin/vercheck.sh
@@ -1,9 +1,19 @@
+echo "Version info"
+echo "git"
 git --version
+echo "Docker"
 docker --version
+echo "docker-machine"
 docker-machine --version
+echo "Vagrant"
 vagrant --version
+echo"Nomad"
 nomad --version
+echo "Ansible"
 ansible --version
+echo "consul"
 consul --version
-terraform --version
+echo "Terraform"
+terraform --versihhon
+echo "VBoxManage"
 vboxmanage --version
